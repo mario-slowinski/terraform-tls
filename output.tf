27a163af39ec1557442c049b4691f4cd0b553274
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-02-23 15:03:48 +0100

    Update output:
    * add "_id" suffix to outputs with IDs
    * rename output cert -> crt

--- output.tf
+++ output.tf
@@ -11,26 +11,26 @@ output "key" {
   sensitive   = true
 }
 
-# terraform output -raw cert
-output "cert" {
+# terraform output -raw crt
+output "crt" {
   value = length(var.ca_crt_pem) > 0 ? (
-    one(tls_self_signed_cert.ca[*].cert_pem)
-    ) : (
     one(tls_locally_signed_cert.crt[*].cert_pem)
+    ) : (
+    one(tls_self_signed_cert.ca[*].cert_pem)
   )
   description = "Certificate in PEM format."
   sensitive   = false
 }
 
-output "key_vault_secret" {
-  value       = one(azurerm_key_vault_certificate.pfx[*].versionless_secret_id)
+output "key_vault_secret_id" {
+  value       = one(azurerm_key_vault_certificate.pfx[*].secret_id)
   description = "Key Vault certificate ID."
   sensitive   = true
 }
 
 
-output "key_vault_certificate" {
-  value       = one(azurerm_key_vault_certificate.pfx[*].versionless_id)
+output "key_vault_certificate_id" {
+  value       = one(azurerm_key_vault_certificate.pfx[*].id)
   description = "Key Vault certificate ID."
   sensitive   = false
 }
