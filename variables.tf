469ae8718b5afe060b1317ad5d408088373dedad
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-05-18 00:34:42 +0200

    Add common variables.

--- variables.tf
+++ variables.tf
@@ -1,3 +1,21 @@
+variable "name" {
+  type        = string
+  description = "Resource's name."
+  default     = ""
+}
+
+variable "separator" {
+  type        = string
+  description = "Single character to separate segments in object's name."
+  default     = "."
+}
+
+variable "space" {
+  type        = string
+  description = "Single character to replace space where required."
+  default     = "_"
+}
+
 variable "tags" {
   type        = map(string)
   description = "Map of tags to be used instead of the ones composed automatically."
