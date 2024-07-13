8187c72041bc6a7e97a6e527d85d079d594cef57
Mariusz Słowiński <mario.slowinski@gmail.com>
2024-07-13 23:46:19 +0200

    Add `var.subject.common_name` because of *for_each* set, its attributes must be accessed on specific instances.

--- pfx.main.tf
+++ pfx.main.tf
@@ -26,8 +26,8 @@ resource "local_file" "crt" {
 resource "null_resource" "pem2pfx" {
   triggers = {
     key = tls_private_key.key.id
-    crt = tls_locally_signed_cert.crt.id
-    ca  = tls_self_signed_cert.ca.id
+    crt = tls_locally_signed_cert.crt[var.subject.common_name].id
+    ca  = tls_self_signed_cert.ca[var.subject.common_name].id
   }
 
   provisioner "local-exec" {
